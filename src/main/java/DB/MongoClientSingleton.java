aeff920d52ac18f24822db08e53b1cf83df9ba54
Benjamin Blankholm <benjamin.blankholm@gmail.com>
2017-08-18 17:06:50 +0200

    mongodb fix?

--- src/main/java/DB/MongoClientSingleton.java
+++ src/main/java/DB/MongoClientSingleton.java
@@ -7,12 +7,11 @@ public class MongoClientSingleton {
   private static MongoClient instance = null;
 
   private MongoClientSingleton(){
-    instance = new MongoClient(new MongoClientURI("mongodb://mongo:27017"));
   }
 
   public static MongoClient getInstance(){
     if(instance == null){
-      instance = new MongoClient();
+      instance = new MongoClient(new MongoClientURI("mongodb://mongodb:27017"));
     }
     return instance;
   }
