cf90824747d93bed12caef7f5f3cbb6b2ed0ca05
Benjamin Blankholm <snurresvup@hotmail.com>
2017-08-19 12:36:14 +0000

    production tweaks

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM java:latest
 ADD target/departure-times-api-1.0-SNAPSHOT.jar /exec/departure-times-api.jar
 ADD service.yml /config/service.yml
+ADD example.keystore example.keystore
 CMD java -jar /exec/departure-times-api.jar server /config/service.yml
 EXPOSE 8080
--- build_and_run.sh
+++ build_and_run.sh
@@ -3,4 +3,4 @@ mvn package
 docker build --no-cache -t departure-times-api:latest .
 docker stop departure-times-api
 docker rm departure-times-api
-docker run --name departure-times-api --link mongodb:mongodb -p 8080:8080 -p 8081:8081 -ti -d departure-times-api
+docker run --name departure-times-api --link mongodb:mongodb -p 8080:8080 -p 8081:8081 -tid departure-times-api
