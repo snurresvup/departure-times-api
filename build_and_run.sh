fd03ac875538f8a6d78f5c251dbcd2c73c1a0122
Benjamin Blankholm <benjamin.blankholm@gmail.com>
2017-08-20 11:11:15 +0200

    Updates Dockerfile

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,5 @@
 mvn clean
 mvn package
 docker build --no-cache -t departure-times-api:latest .
-docker stop departure-times-api
-docker rm departure-times-api
+docker rm -f departure-times-api
 docker run --restart always --name departure-times-api --link mongodb:mongodb -p 8080:8080 -p 8081:8081 -tid departure-times-api
